fc8766bc11ba45d6b3730c5444cb449b077dc3d7
Ibrahim Eksi <95856789+ibraeksi@users.noreply.github.com>
2025-11-26 20:13:31 +0100

    added updated app

--- movie_plot_recommender/elevatorpitchapp.py
+++ movie_plot_recommender/elevatorpitchapp.py
@@ -6,6 +6,7 @@ from scipy import sparse
 import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
+nltk.download('wordnet')
 from pathlib import Path
 
 fitted_count = Path(__file__).parents[0] / 'models/count_vectorizer_v01.pkl'
